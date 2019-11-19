4dfe54eb893667e875a544f17d62e1173f64caf7
Bendik <bendikbo@github.com>
2019-11-19 09:40:23 +0100

    game code mostly finished

---
+++ game-1.0/inc/snake.h
@@ -0,0 +1,39 @@
+
+typedef enum Direction Direction;
+enum Direction
+{
+    Up = 0,
+    Down = 1,
+    Left = 2,
+    Right = 3
+};
+
+typedef struct snake_square square;
+struct snake_square
+{
+    int x;
+    int y;
+    square* next;
+    square* previous;
+};
+
+
+typedef snake snake;
+struct snake
+{
+    square* head; //pointer to head square of snake body
+    square* back; //pointer to back square
+    int size;
+};
+
+
+//other module functions
+
+
+void spawn_apple(snake* snake);
+
+//starts the snake game
+int snake_game(int fps);
+
+//ends the snake game
+void end_game(int score);
