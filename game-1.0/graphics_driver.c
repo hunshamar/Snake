559e9414333a244f6cfeb71f70b4ff85d039d533
Asgeir <asgeirhuns@mail.com>
2019-11-22 18:37:30 +0100

    Finished code

--- game-1.0/graphics_driver.c
+++ game-1.0/graphics_driver.c
@@ -5,29 +5,26 @@
 #define white_color 0xffff
 
 
-#include "fonts.h"
-
- 
-#include <sys/types.h>
-#include <sys/wait.h>
 
 static uint16_t graphic_memory;
 static uint16_t* fbp;
-static uint16_t reduced_resolution_graphics_array[24][24];
-static uint16_t text_array[80][240];
-static char     char_array[10][30];
+static uint16_t reduced_resolution_graphics_array[24][24]; // Reduced grid for playing snake. 
+static uint16_t text_array[80][240]; // Part of screen to print text and game score
+static char     char_array[10][30]; // Holds the characters to print
 
 static uint16_t* fbp;
 
-bool get_bit(char c, int n){
-	return c & 1 << n;	
-}
+
 
 void draw_pixel(int x, int y, uint16_t color){
 	reduced_resolution_graphics_array[x][y] = color;
 	update_screen(80+x*10,y*10,10,10);
 }
 
+/* 
+ Initializes the char_array to only hold blank values by default.
+ The text_array is sett to black on all pixels by default. 
+*/
 void text_array_init()
 {
 	for (int y = 0; y < 30; y++)
@@ -47,23 +44,35 @@ void text_array_init()
 	}	
 }
 
-void print_char(int x, int y, char c)
-{
-	char_array[x][y] = c;	
-}
 
+/*
+	Prints a character to the screen 
+*/
 void print_string(int x, int y, char* string)
 {
 	int i = 0;
 	while(string[i]){
-		print_char(x+i,y,string[i]);
+		char_array[x+i][y] = string[i];
 		i++;
 	}
-	update_char_array();
+	update_text_array();
 	update_screen(x*8,y*8,i*8,8);
 }
 
-void update_char_array()
+
+
+bool get_bit(char c, int n)
+{
+	return c & 1 << n;	
+}
+
+/* 
+	Updates the text array with correct pixels to print the characters from the character array
+	Uses the font.h file to get the 8x8 pixel grid for the characters from the char_array and writes the character
+	in the correct format to the text_array
+
+ */
+void update_text_array()
 {
 	int x_char = 0;
 	int y_char = 0;
@@ -127,10 +136,9 @@ void graphics_init()
  	
 	print_string(2,4,"Score:");
 
-    printf("Init graphics");  
 
-
-    uint16_t color = grass_color;
+	/* Print the grass for the snake game on the 24x24 grid */
+    uint16_t color = grass_color;	
 	for (int y = 0; y < 24; y++)
 	{
 		for (int x = 0; x < 24; x++)
@@ -160,11 +168,17 @@ void update_score(int score){
     print_string(2,6,score_buffer);
 }
 
-
+/*
+ 	Dynamically updates the screen to only draw the newly updated pixels.
+	screen_x (from 0 to 319) and screen_y (from 0 to 239) is the pixel to write from
+	Width and height creates a rectangle which decides which parts of the screen to update. 
+	THe function reads from either the text_array or the 24x24 reduced_resolution_graphics array
+	depending on the rectangle to update. 
+*/
 
 void update_screen(int screen_x, int screen_y, int width, int height)
 {
-		int x_game_grid = (screen_x-80)/10;
+		int x_game_grid = (screen_x-80)/10;  
 		int y_game_grid = screen_y/10;
 		int k = 320*screen_y + screen_x;
 
@@ -195,10 +209,9 @@ void update_screen(int screen_x, int screen_y, int width, int height)
 	refresh_framebuffer(screen_x, screen_y, width, height);		
 }
 
-
-
-
-
+/*
+	Wwrites from the graphics memory to the screen
+*/
 void refresh_framebuffer(int x, int y, int width, int height)
 {
 	struct fb_copyarea rect;
--- game-1.0/graphics_driver.h
+++ game-1.0/graphics_driver.h
@@ -1,10 +1,10 @@
+#ifndef GRAPHICS_H_
+#define GRAPHICS_H
+
 #include <stdint.h>
 #include <sys/mman.h>
-
-
-
-
-
+#include "fonts.h"
+#include <sys/types.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
@@ -18,20 +18,19 @@
 #include <string.h>
 
 void graphics_init();
-void refresh_framebuffer();
+void refresh_framebuffer(int x, int y, int width, int height);
 
 void update_screen(int screen_x, int screen_y, int width, int height);
-void map_reduced_to_full();
 void print_string(int x, int y, char* string);
 
-
-
-void refresh_score_display();
 void refresh_reduced_grid(int x, int y, int width, int height);
 void refresh_text_display();
 
 void draw_pixel(int x, int y, uint16_t color);
-void update_char_array();
+void update_text_array();
 
 
 void update_score(int score);
+
+
+#endif
