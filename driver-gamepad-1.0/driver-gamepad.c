5f2a289120ce0699c32d4fd2a8dfefb4c2af538a
Asgeir <asgeirhuns@mail.com>
2019-11-22 15:06:29 +0100

    wtf

--- driver-gamepad-1.0/driver-gamepad.c
+++ driver-gamepad-1.0/driver-gamepad.c
@@ -1,4 +1,33 @@
-#include"driver-gamepad.h"
+
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/fs.h> // Necessary functions to optain device numbers
+#include <linux/ioport.h> 
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/moduleparam.h>
+#include <linux/kdev_t.h>
+#include <asm/io.h>
+#include <asm/uaccess.h>
+#include "efm32gg.h"
+
+#define NAME "gamepad"
+#define COUNT 1
+
+static int __init gamepad_init(void);
+static void __exit gamepad_cleanup(void);
+static int gamepad_open(struct inode* inode, struct file* filp);
+static int gamepad_release(struct inode* inode, struct file* filp);
+static ssize_t gamepad_read(struct file* filp, char* __user buff, size_t count, loff_t* offp);
+static ssize_t gamepad_write(struct file* filp, char* __user buff, size_t count, loff_t* offp);
+
+/* Static variables */
+static dev_t dev;
+struct cdev cdev;
+struct class* cl;
+
 
 
 module_init(gamepad_init);
@@ -52,12 +81,7 @@ static int __init gamepad_init(void)
     /* Allocate and initialize cdev structure */
     cdev_init(&cdev, &my_fops);
     cdev.owner = THIS_MODULE;
-    int err = cdev_add(&cdev, dev, COUNT);
-    if (err){
-        printf(KERN_NOTICE "Error %d adding gamepad", err);
-        return -1;
-    }
-        
+    cdev_add(&cdev, dev, COUNT);
     
     cl = class_create(THIS_MODULE, NAME);
     device_create(cl, NULL, dev, NULL, NAME);
